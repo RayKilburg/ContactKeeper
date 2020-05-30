b8d4ad7ae7027ff708a86e7cd421e8554e19eab8
RayKilburg <rkilburg42@gmail.com>
2020-05-30 03:15:56 -0400

    Delete Contact From UI

--- client/src/context/contact/contactReducer.js
+++ client/src/context/contact/contactReducer.js
@@ -7,6 +7,7 @@ import {
   FILTER_CONTACT,
   CLEAR_FILTER,
 } from "../types";
+import contactContext from "./contactContext";
 
 export default (state, action) => {
   switch (action.type) {
@@ -14,6 +15,13 @@ export default (state, action) => {
       return {
         ...state,
         contacts: [...state.contacts, action.payload],
+      };
+    case DELETE_CONTACT:
+      return {
+        ...state,
+        contacts: state.contacts.filter(
+          (contact) => contact.id !== action.payload
+        ),
       };
     default:
       return state;
